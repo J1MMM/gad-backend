7a582fa03078d77a1eba5d6e94fe392864fe7c19
J1MMM <jimuelbaraero00@gmail.com>
2025-02-07 07:58:45 +0800

    07/02/25

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://spctmo.vercel.app",
   "https://vercel.com",
   "http://localhost:5173",
+  "https://gad-seven.vercel.app",
 ];
 
 module.exports = allowedOrigins;
